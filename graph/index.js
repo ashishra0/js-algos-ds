a8826d60ecac9a4b649fdaa738d0da4e2a73b559
Ashish Rao <ashishrao2598@gmail.com>
2019-09-14 10:57:37 +0530

    fix code typos

--- graph/index.js
+++ graph/index.js
@@ -5,7 +5,6 @@ class Graph {
   
   addNode(node) {
     this.adjList[node.value] = {
-      node: node,
       edges: []
     }
   }
@@ -21,16 +20,10 @@ class Graph {
     nodes.forEach(currNode => {
       const edges = this.adjList[currNode].edges
       if (edges.indexOf(node) > -1) { // this returns -1 if there is no value match.
-        edges.splice(index, 1)
+        edges.splice(node, 1)
       }
     });
   }
 }
 
-const graph = new Graph();
-const node1 = {value: 1}
-const node2 = {value : 2}
-graph.addNode(node1)
-graph.addNode(node2)
-graph.addEdge(node1, node2);
-console.log(graph)
+module.exports = { Graph };
